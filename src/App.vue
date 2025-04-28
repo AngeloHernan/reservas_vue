d8f5b097d488b428e568c17492805aecbb0a7051
Angelo <angelohernan@gmail.com>
2025-04-28 01:17:02 -0400

    Fix Menu

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,76 @@
   import { h, ref } from "vue";h
 
   const nombre = 'Vue!';
-  const styleColor = "color: blue";
-  const cabeceras = ['Horario', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'];
 
-  const nombre_docente = ref("")
+  const docente = ref("")
+  const curso = ref("")
+  
+  //Fecha del mes
+  const fecha = new Date();
+  const mesActual = ref(fecha.getMonth())
+  const mes = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 
-  const counter = ref(0);
-  const e = () => {
-    counter.value ++;
+  const texto = ref("");
+  const selecBoton = () => {
+    texto.value = "Reserva Realizada con Exito."
   }
+  
+  
+  
+
+  const nombreMes = () =>{
+    mesActual.value = fecha.getMonth(); //0 Enero, 1 Febrero ... 
     
+  }
+
+  const fechaSemanal = () =>{
+    const diaDeLaSemana = fechaActual.getDay(); //0 domingo, 1 lunes...
+    const semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    
+  }
+
 </script>
 
 <template>
   
   <h1>Reservas de Laboratorios</h1>
-  
-  <div></div>
+  <h2>{{ mes[mesActual] }}</h2>
   
   <div .class="box-select">
     
+    
+    <select name="Laboratorio">
+      <option value="lab1" selected>Laboratorio Primer Piso </option>
+      <option value="lab2">Laboratorio Segundo Piso</option>
+    </select>
+    <p>Elegir Laboratorio a reservar</p>
+    
     <select name="Selecciona Dia">
-      <option value="Lunes">Lunes</option>
+      <option value="Lunes" selected>Lunes</option>
       <option value="Martes">Martes</option>
       <option value="Miercoles">Miercoles</option>
       <option value="Jueves">Jueves</option>
       <option value="Viernes">Viernes</option>
     </select>
+    <p>Elegir dia a reservar</p>
 
-    <select name="select">
-      <option value="value1">Value 1</option>
-      <option value="value2" selected>Value 2</option>
-      <option value="value3">Value 3</option>
+    <select name="Selecciona Horario">
+      <option value="bloque1" selected>08:00 - 09:30</option>
+      <option value="bloque2">09:45 - 11:15</option>
+      <option value="bloque3">11:30 - 13:00</option>
+      <option value="bloque4">14:00 - 15:30</option>
     </select>
-
+    <p>Elegir bloque a reservar</p>
+  
+    <input v-model="docente" placeholder="Nombre" />
     <p>Nombre docente</p>
-    <input v-model="nombre_docente" placeholder="Nombre" />
-     
+
+    <input v-model="curso" placeholder="Curso" />
+    <p>Curso</p>
+
     <div>
-      <button @click="e">Add 1</button>
-      <h2>Se ha hecho clic en el botón de arriba {{ counter }} veces.</h2>
+      <button @click="selecBoton">Reservar</button>
+      <h2>{{ texto }}</h2>
     </div>
 
   </div>
@@ -49,14 +79,16 @@
   <table .class="box-tabla">
     <tbody>
       <tr>
-        <th
-          v-for="(dia, i) in cabeceras" :key="dia">
-          {{ dia }}
-        </th>
+        <th>Horario</th>
+        <th>Lunes</th>
+        <th>Martes</th>
+        <th>Miercoles</th>
+        <th>Jueves</th>
+        <th>Viernes</th>
       </tr>
       <tr>
         <td>08:00 - 09:30</td>
-        <td>Hola!!</td>
+        <td>Hola Lunes</td>
         <td>Hola martes</td>
         <td>Hola Miercoles</td>
         <td>Hola Jueves</td>
@@ -64,7 +96,7 @@
       </tr>
       <tr>
         <td>09:45 - 11:15</td>
-        <td>Hola!!</td>
+        <td>Hola Lunes</td>
         <td>Hola martes</td>
         <td>Hola Miercoles</td>
         <td>Hola Jueves</td>
@@ -72,7 +104,7 @@
       </tr>
       <tr>
         <td>11:30 - 13:00</td>
-        <td>Hola!!</td>
+        <td>Hola Lunes</td>
         <td>Hola martes</td>
         <td>Hola Miercoles</td>
         <td>Hola Jueves</td>
@@ -80,7 +112,7 @@
       </tr>
       <tr>
         <td>14:00 - 15:30</td>
-        <td>Hola!!</td>
+        <td>Hola Lunes</td>
         <td>Hola martes</td>
         <td>Hola Miercoles</td>
         <td>Hola Jueves</td>
@@ -117,4 +149,7 @@ td {
   text-align: center;
 }
 
+h2{
+  position: relative;
+}
 </style>
